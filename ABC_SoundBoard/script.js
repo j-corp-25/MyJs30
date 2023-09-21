7cc2e45e5509e59fb718a59fc5ee94e24fad2ef4
Jordy <jordycorporan95@gmail.com>
2023-09-21 10:52:57 -0400

    Tested keydowns

--- ABC_SoundBoard/script.js
+++ ABC_SoundBoard/script.js
@@ -1,4 +1,10 @@
 
 window.addEventListener('keydown',function(e) {
-    console.log(e)
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
+    console.log(audio)
+    if(!audio) return;
+    audio.currentTime = 0;
+    audio.play();
+    console.log(key)
 })
